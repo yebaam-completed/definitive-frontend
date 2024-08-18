8c253cd929f5e09e4797aa2e4b15e68ace2ce9ff
flowercordoba <flowercordoba7@gmail.com>
2024-08-18 15:00:40 -0500

    photo cover

--- src/features/user/components/CoverUserPhoto.tsx
+++ src/features/user/components/CoverUserPhoto.tsx
@@ -3,16 +3,16 @@ import React, { useState } from 'react';
 
 const CoverUserPhoto: React.FC = () => {
 
-  
+
   return (
     <div className="position-relative">
       <img
-        src="https://images.unsplash.com/photo-1723647395168-d916159b78c9?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"  
+        src="https://images.unsplash.com/photo-1723647395168-d916159b78c9?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"  // Aquí debes poner el link a la foto de portada real.
         alt="Cover"
         className="img-fluid w-100"
+        style={{ height: '300px', objectFit: 'cover', cursor: 'pointer' }} 
     
       />
- 
 
     </div>
   );
